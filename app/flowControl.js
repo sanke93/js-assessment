ee5912f628ecdeea84c4db3f8905c869dfba85bd
Sanket Chauhan <sanke93@gmail.com>
2013-12-31 11:45:39 -0500

    flow control tests

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,6 +11,19 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      if(num%3 === 0){
+        if(num%5 === 0)
+          return "fizzbuzz";
+        else
+          return "fizz";
+      }
+      else if(num%5 === 0)
+        return "buzz";
+      else if(num == undefined || isNaN(num))
+        return false;
+      else 
+        return num;
+
     }
   };
 });
