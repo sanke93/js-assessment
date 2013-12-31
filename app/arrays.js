4665b2f61fd4006a8bdba5788be97bb615fb463f
Sanket Chauhan <sanke93@gmail.com>
2013-12-31 11:35:20 -0500

    solved 4 tests

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        for(var i=0; i<arr.length; i++){
+            if(arr[i] == item){
+                return i;
+            }
+        }
+        return -1;
     },
 
     sum : function(arr) {
-
+        var sum=0;
+        for(var i=0; i<arr.length; i++){
+            sum += arr[i];
+        }       
+        return sum;
     },
 
     remove : function(arr, item) {
-
+        for(var i=0; i<arr.length; i++){
+            if(arr[i] == item){
+                arr.splice(i,1);
+                return arr;
+            }
+        }
     },
 
     removeWithoutCopy : function(arr, item) {
@@ -19,19 +33,23 @@ define(function() {
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift(arr);
+        return arr;
     },
 
     concat : function(arr1, arr2) {
@@ -39,11 +57,17 @@ define(function() {
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index,0,item);
     },
 
     count : function(arr, item) {
-
+        var sum=0;
+        for (var i=0; i<arr.length; i++){
+            if(arr[i]==item){
+                sum++;
+            }
+        }
+        return sum;
     },
 
     duplicates : function(arr) {
@@ -51,7 +75,10 @@ define(function() {
     },
 
     square : function(arr) {
-
+        for(var i=0; i<arr.length; i++){
+            arr[i] = arr[i]*arr[i];
+        }
+        return arr;
     },
 
     findAllOccurrences : function(arr, target) {
--- app/numbers.js
+++ app/numbers.js
@@ -15,7 +15,7 @@ define(function() {
     },
 
     multiply: function(a, b) {
-
+        return a*b;
     }
   };
 });
